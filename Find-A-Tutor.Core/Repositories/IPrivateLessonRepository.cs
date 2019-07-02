96e72733f6fece5c2ce9dd436ca66274fcfef155
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-07-02 19:22:36 +0200

    Added repository interfaces: event and user.

---
+++ Find-A-Tutor.Core/Repositories/IPrivateLessonRepository.cs
@@ -0,0 +1,17 @@
+﻿using Find_A_Tutor.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Find_A_Tutor.Core.Repositories
+{
+    public interface IPrivateLessonRepository
+    {
+        Task<PrivateLesson> GetAsync(Guid id);
+        Task<PrivateLesson> GetAsync(SchoolSubject name); //todo: am I sure?
+        Task<IEnumerable<PrivateLesson>> BrowseAsync(string description = "");
+        Task AddAsync(PrivateLesson privateLesson);
+        Task UpdateAsync(PrivateLesson privateLesson);
+        Task DeleteAsync(PrivateLesson privateLesson);
+    }
+}
---
+++ Find-A-Tutor.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Find_A_Tutor.Core.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace Find_A_Tutor.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(string email);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
