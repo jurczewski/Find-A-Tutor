f16f4bab52f1cf13299801e30a84377479d06b23
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-07-02 16:22:12 +0200

    Travis added, Entity domain class added

---
+++ Find-A-Tutor.Core/Domain/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Find_A_Tutor.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
