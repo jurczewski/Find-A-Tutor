13812ab2435cc6897d20aaaf30a42adfd8d17329
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-07-07 19:41:55 +0200

    PrivateLesson DTO updated

--- Find-A-Tutor.Infrastructure/DTO/PrivateLessonDTO.cs
+++ Find-A-Tutor.Infrastructure/DTO/PrivateLessonDTO.cs
@@ -5,17 +5,16 @@ namespace Find_A_Tutor.Infrastructure.DTO
     public class PrivateLessonDTO
     {
         public Guid Id { get; set; }
-        public Guid StudnetId { get; protected set; }
-        public Guid? TutorId { get; protected set; }
-        //todo: odkomentować i przetestować
-        //public Guid CreatedAt { get; protected set; } 
-        public DateTime? TakenAt { get; protected set; }
-        public DateTime UpdateAt { get; protected set; }
-        public DateTime RelevantTo { get; protected set; }
-        public string Description { get; protected set; }
-        public string Subject { get; protected set; }
-        public bool Taken => TutorId.HasValue;
-        public bool IsPaid { get; protected set; }
-        public bool IsDone { get; protected set; }
+        public Guid StudnetId { get; set; }
+        public Guid? TutorId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? TakenAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public DateTime RelevantTo { get; set; }
+        public string Description { get; set; }
+        public string Subject { get; set; }
+        public bool IsAssigned => TutorId.HasValue;
+        public bool IsPaid { get; set; }
+        public bool IsDone { get; set; }
     }
 }
