e5a39f187d69341b6d2be38ffd4ba157861b6a1e
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-07-19 16:35:21 +0200

    first attmpet with tests (end to end)

---
+++ Find-A-Tutor.Tests.EndToEnd/Controllers/AccountControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Find_A_Tutor.Infrastructure.Commands.Users;
+using System.Net;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+using Find_A_Tutor.Infrastructure.DTO;
+using Newtonsoft.Json;
+
+namespace Find_A_Tutor.Tests.EndToEnd.Controllers
+{
+    public class AccountControllerTests : ControllerTestsBase
+    {
+        [Fact]
+        public async Task given_valid_non_existing_user_he_should_be_register()
+        {
+            var command = new Register
+            {
+                Role = "student",
+                FirstName = "test",
+                LastName = "testowy",
+                Email = "test@test.com",
+                Password = "a12345678"
+            };
+            var payload = GetPayload(command);
+            var response = await Client.PostAsync("account/register", payload);
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.Created);
+        }
+
+        [Fact]
+        public async Task given_unique_email_user_should_be_created()
+        {
+            var command = new Register
+            {
+                Role = "student",
+                FirstName = "test",
+                LastName = "testowy",
+                Email = "test@test.com",
+                Password = "a12345678"
+            };
+            var payload = GetPayload(command);
+            var response = await Client.PostAsync("account/register", payload);
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.Created);
+            response.Headers.Location.ToString().Should().BeEquivalentTo($"account/{command.Email}");
+
+            var user = await GetUserAsync(command.Email);
+            user.Email.Should().BeEquivalentTo(command.Email);
+        }
+
+        private async Task<AccountDto> GetUserAsync(string email)
+        {
+            var response = await Client.GetAsync($"account/{email}");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<AccountDto>(responseString);
+        }
+    }
+}
---
+++ Find-A-Tutor.Tests.EndToEnd/Controllers/ControllerTestsBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Text;
+using Find_A_Tutor.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+
+namespace Find_A_Tutor.Tests.EndToEnd.Controllers
+{
+    public abstract class ControllerTestsBase
+    {
+        protected readonly TestServer Server;
+        protected readonly HttpClient Client;
+
+        protected ControllerTestsBase()
+        {
+            Server = new TestServer(new WebHostBuilder()
+                          .UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        protected static StringContent GetPayload(object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+    }
+}
