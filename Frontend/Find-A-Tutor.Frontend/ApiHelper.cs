b8a167e9eacc13eebb20bf350e5ba6501598e3d7
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-10-03 15:18:38 +0200

    project init

---
+++ Frontend/Find-A-Tutor.Frontend/ApiHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Find_A_Tutor.Frontend
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
