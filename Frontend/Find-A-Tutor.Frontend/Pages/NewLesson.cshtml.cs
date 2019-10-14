582a5e4f5e66c1cb0396351694cc1146881c8c2c
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-10-15 00:50:06 +0200

    New lesson Page added

---
+++ Frontend/Find-A-Tutor.Frontend/Pages/NewLesson.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Find_A_Tutor.Frontend.Pages
+{
+    public class NewLessonModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
