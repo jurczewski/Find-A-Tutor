d87807b877b171ba1226198fa2416f73bad2d122
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-11-08 14:22:06 +0100

    [ADDED] Ping controller for Api

---
+++ Backend/Find-A-Tutor.Api/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Find_A_Tutor.Api.Controllers
+{
+    [Route("[controller]")]
+    public class PingController : ApiControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("pong");
+        }
+    }
+}
