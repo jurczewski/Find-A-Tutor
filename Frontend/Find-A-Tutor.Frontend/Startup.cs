b5d54ddb928364d9148d88813ece93fb6d159a42
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-11-08 15:06:09 +0100

    [ADDED] HealthCheck Added - API ping

--- Frontend/Find-A-Tutor.Frontend/Startup.cs
+++ Frontend/Find-A-Tutor.Frontend/Startup.cs
@@ -1,11 +1,16 @@
 using Find_A_Tutor.Frontend.Services;
-using Find_A_Tutor.Frontend.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Mime;
 
 namespace Find_A_Tutor.Frontend
 {
@@ -21,18 +26,21 @@ namespace Find_A_Tutor.Frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
-            services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
             services.AddSession();
             services.AddMemoryCache();
             services.AddHttpContextAccessor();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var apiUrl = Configuration.GetSection("ApiUrl");
+            services.AddHealthChecks()
+                    .AddUrlGroup(new Uri($"{apiUrl?.Value}/ping"), "Find-A-Tutor.Api");
 
             ApiHelper.InitializeClient();
             services.AddSingleton<IPrivateLessonService, PrivateLessonService>();
@@ -53,6 +61,22 @@ namespace Find_A_Tutor.Frontend
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //HealthChecks
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    var result = JsonConvert.SerializeObject(
+                        new
+                        {
+                            status = report.Status.ToString(),
+                            details = report.Entries.Select(e => new { key = e.Key, value = Enum.GetName(typeof(HealthStatus), e.Value.Status) })
+                        });
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                    await context.Response.WriteAsync(result);
+                }
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
