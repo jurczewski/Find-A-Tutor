1c6e5166677b35ec5212e51bd7f12bdb663f45bd
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-11-08 19:59:57 +0100

    Ping chagned to HomeController and added emtpy Get

---
+++ Backend/Find-A-Tutor.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Find_A_Tutor.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : ApiControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Find-A-Tutor.Api");
+        }
+    }
+}
