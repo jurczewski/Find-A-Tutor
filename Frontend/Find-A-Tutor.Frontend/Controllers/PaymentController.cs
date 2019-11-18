cd018b9f97788b551cea8dc038b13545d8059151
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-11-18 20:24:15 +0100

    Added controller, used after completed paypal transaction + model

---
+++ Frontend/Find-A-Tutor.Frontend/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using Find_A_Tutor.Frontend.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Find_A_Tutor.Frontend.Controllers
+{
+    [Route("")]
+    public class PaymentController : Controller
+    {
+        [HttpPost("pay")]
+        public async Task<Result> UpdatePaymentStatusToPaid([FromBody]PaymentDetails paymentDetails)
+        {
+            var url = "http://localhost:5000" + "/Payment/paypal-transaction-complete";
+            //var token = _accessor.HttpContext.Session.GetString("token");
+
+            //ApiHelper.ApiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            using (var response = await ApiHelper.ApiClient.PostAsJsonAsync(url, paymentDetails))
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    var result = await response.Content.ReadAsAsync<ResultSimple>();
+                    return Result.Error(result.Errors.ToArray());
+                }
+                else
+                {
+                    return Result.Ok();
+                }
+            }
+        }
+    }
+}
---
+++ Frontend/Find-A-Tutor.Frontend/Model/PaymentDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Find_A_Tutor.Frontend.Model
+{
+    public class PaymentDetails
+    {
+        public string OrderID { get; set; }
+        public Guid PrivateLessonId { get; set; }
+    }
+}
