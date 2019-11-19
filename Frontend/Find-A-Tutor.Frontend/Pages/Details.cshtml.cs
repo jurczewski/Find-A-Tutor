e271fa8204658049c5bb35591bb7f569d0309a4b
Bartosz Jurczewski <Bartosz.Jurczewski@rossmann.com.pl>
2019-11-19 15:00:11 +0100

    paypal credentials moved to appsettings

--- Frontend/Find-A-Tutor.Frontend/Pages/Details.cshtml.cs
+++ Frontend/Find-A-Tutor.Frontend/Pages/Details.cshtml.cs
@@ -3,17 +3,20 @@ using System.Threading.Tasks;
 using Find_A_Tutor.Frontend.Model;
 using Find_A_Tutor.Frontend.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace Find_A_Tutor.Frontend.Pages
 {
     public class DetailsModel : PageModel
     {
         private readonly IPrivateLessonService _privateLessonService;
+        public readonly string _payPalClientId;
         public Result<PrivateLesson> privateLesson;
 
-        public DetailsModel(IPrivateLessonService privateLessonService)
+        public DetailsModel(IPrivateLessonService privateLessonService, IConfiguration configuration)
         {
             _privateLessonService = privateLessonService;
+            _payPalClientId = configuration.GetValue<string>("PayPal:client-id");
         }
 
         public async Task OnGet(Guid id)
